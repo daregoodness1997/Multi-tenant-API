60260f47df76ff126a55d3401a6f4fa9b3b61b89
daregoodness1997 <daregoodness@gmail.com>
2022-06-23 21:36:11 +0100

    tenant db setup

--- db/adminDB.js
+++ db/adminDB.js
@@ -1,2 +1,27 @@
 const connectDB = require('connect');
 const mongoose = require('mongoose');
+
+const url = `${process.env.MONGO_URL}/adminDB`;
+
+let db;
+
+const tenantSchema = new mongoose.Schema(
+  {
+    id: { type: String },
+    name: { type: String },
+  },
+  { timestamps: true }
+);
+
+const tenantModel = mongoose.model('Tenants', tenantSchema);
+
+const getDB = async () => {
+  return db ? db : await connectDB(url);
+};
+
+const getTenantModel = async () => {
+  const adminDB = await getDB();
+  return adminDB.model('Tenants', tenantSchema);
+};
+
+module.exports = { getTenantModel };
