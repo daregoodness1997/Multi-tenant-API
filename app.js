1d9ebe0bcd524e5476b2b0f253064f987ee20116
daregoodness1997 <daregoodness@gmail.com>
2022-06-23 20:10:32 +0100

    server setup

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 require('http-status-codes');
 require('dotenv');
+
+const express = require('express');
+const app = express();
+
+const start = () => {
+  try {
+    app.listen(6600, (req, res) => {
+      console.log('listening on at port 6600....');
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
